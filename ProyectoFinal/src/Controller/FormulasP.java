7df7d0076dd7a16a4bfe24f8124712c82ae2b19c
Ronxl2005 <ronaljumbo9@gmail.com>
2024-12-03 18:22:08 -0500

    Update FormulasP.java

--- ProyectoFinal/src/Controller/FormulasP.java
+++ ProyectoFinal/src/Controller/FormulasP.java
@@ -1,88 +1,134 @@
 package controller.ejercicios;
-public class FormulasProyecto {
-    public double gramostoUnidades (double gramos) {
-        double gTokg =gramos/1000;
-        double gTolb = gramos/ 453.592;
-        double gTooz = gramos/ 28.3495;
-        double gTotazas = gramos/ 236.588;
-        double gTocda = gramos/ 15;
-        double gTocdta = gramos/ 4.92892;
-        double gTotoneladas = gramos/ 1000000;
-    return gTokg; return gTolb; return gTooz; return gTotazas; return gTocda; return gTocdta; return gTotoneladas;
+
+public class ConversionResult {
+    public double g;
+    public double kg;
+    public double lb;
+    public double oz;
+    public double tazas;
+    public double cda;
+    public double cdta;
+    public double toneladas;
+    public double ml;
+    public double gal;
+    public double lt;
+
+    public ConversionResult gramostoUnidades(double gramos){
+        ConversionResult result = new ConversionResult();
+        result.kg = gramos/ 1000;
+        result.lb = gramos/ 453.592;
+        result.oz = gramos/ 28.3495;
+        result.tazas = gramos/ 236.588;
+        result.cda = gramos/ 15;
+        result.cdta = gramos/ 4.92892;
+        result.toneladas = gramos/ 1000000;
+
+        return result;
+    }
+    public ConversionResult kgtoUnidades(double kg){
+        ConversionResult result = new ConversionResult();
+        result.g = kg * 1000;
+        result.lb = kg * 2.20462;
+        result.oz = kg * 35.274;
+        result.tazas = kg * 7.8125;
+        result.cda = kg * 66.6667;
+        result.cdta = kg * 200;
+        result.toneladas = kg * 1000;
+
+        return result;
+    }
+    public ConversionResult lbtoUnidades(double lb){
+        ConversionResult result = new ConversionResult();
+        result.kg = lb / 2.20462;
+        result.g = lb * 453.592;
+        result.oz = lb * 16;
+        result.tazas = lb / 1.91722;
+        result.cda = lb / 30.6667;
+        result.cdta = lb * 92;
+        result.toneladas = lb / 2204.62;
+
+        return result;
+    }
+    public ConversionResult oztoUnidades(double oz){
+        ConversionResult result = new ConversionResult();
+        result.kg = oz /35.274;
+        result.lb =oz/ 16;
+        result.g =oz *28.3495;
+        result.tazas =oz /4.5;
+        result.cda =oz / 0.5333;
+        result.cdta =oz / 0.1667;
+        result.toneladas =oz /35274;
+
+        return result;
+    }
+    public ConversionResult tazastoUnidades(double tazas){
+        ConversionResult result = new ConversionResult();
+        result.kg = tazas / 7.8125;
+        result.lb = tazas * 0.537;
+        result.oz = tazas * 4.5;
+        result.g = tazas * 128;
+        result.cda = tazas * 16;
+        result.cdta = tazas * 48;
+        result.toneladas = tazas / 7812.5;
+
+        return result;
+    }
+    public ConversionResult cdatoUnidades(double cda){
+        ConversionResult result = new ConversionResult();
+        result.kg = cda / 66.6667;
+        result.lb = cda / 30.6667;
+        result.oz = cda / 0.5333;
+        result.tazas = cda / 16;
+        result.g = cda * 15;
+        result.cdta = cda * 3;
+        result.toneladas = cda / 666666.7;
+
+        return result; 
+    }
+    public ConversionResult cdtatounidades(double cdta){
+        ConversionResult result = new ConversionResult();
+        result.kg = cdta / 200;
+        result.lb = cdta / 92;
+        result.oz = cdta / 0.1667;
+        result.tazas = cdta / 48;
+        result.cda = cdta / 3;
+        result.g = cdta * 5;
+        result.toneladas = cdta / 200000;
+
+        return result;
+    }
+    public ConversionResult toneladastoUnidades(double toneladas){
+        ConversionResult result = new ConversionResult();
+        result.kg = toneladas * 1000;
+        result.lb = toneladas * 2204.62;
+        result.oz = toneladas * 35274;
+        result.tazas = toneladas * 7812.5;
+        result.cda = toneladas * 66666.7;
+        result.cdta = toneladas * 200000;
+        result.g = toneladas * 2000000;
+
+        return result;
+    }
+    public ConversionResult lttoUnidades(double lt){
+        ConversionResult result = new ConversionResult();
+        result.ml = lt * 1000;
+        result.gal = lt * 0.264172;
+        
+        return result;
+    }
+    public ConversionResult mltoUnidades(double ml){
+        ConversionResult result = new ConversionResult();
+        result.lt = ml / 1000;
+        result.gal = ml /  378541;
+
+        return result;
+    }
+    public ConversionResult galtoUnidades(double gal){
+        ConversionResult result = new ConversionResult();
+        result.lt = gal * 3.78541;
+        result.ml = gal * 3785.41;
+
+        return result;
     }
 
-    public static double kgtoUnidades (double kg ) {
-        double kgTog = kg * 1000;
-        double kgTolb = kg * 2.20462;
-        double kgTooz = kg * 35.274;
-        double kgTotaza = kg * 7.8125;
-        double kgTocda = kg * 66.6667;
-        double kgTocdta = kg * 200;
-        double kgTotoneladas = kg / 1000;
-
-        return kgTog; return kgTolb; return kgTooz; return kgTotaza; return kgTocda; return kgTocdta; return kgTotoneladas;
-    }
-    public double lbToUnidades ( double lb){
-        double lbTokg =lb /2.20462;
-        double lbTog =lb*453.592;
-        double lbTokoz =lb *16;
-        double lbTotaza =lb /1.91722;
-        double lbTocda =lb * 30.6667;
-        double lbTocdta =lb * 92;
-        double lbTotoneldas =lb /2204.62;
-        return lbTokg; return lbTog; return lbTokoz; return lbTotaza; return lbTocda; return lbTocdta; return lbTotoneldas;
-    }
-    public double ozToUnidades ( double oz){
-        double ozTokg = oz /35.274;
-        double ozTolb =oz/ 16;
-        double ozTog =oz *28.3495;
-        double ozTotaza =oz /4.5;
-        double ozTocda =oz / 0.5333;
-        double ozTocdta =oz / 0.1667;
-        double ozTotoneldas =oz /35274;
-        return ozTokg; return ozTolb; return ozTog; return ozTotaza; return ozTocda; return ozTocdta; return ozTotoneldas;
-    }
-    public double tazasToUnidades (double tazas){
-        double tazasTokg = tazas/7.8125;
-        double tazasTolb = tazas*0.537;
-        double tazasTooz = tazas*4.5;
-        double tazasTog = tazas*128;
-        double tazasTocda = tazas*16;
-        double tazasTocdta = tazas*48;
-        double tazasTotonaladas = tazas/7812.5;
-        return tazasTokg, tazasTolb, tazasTooz, tazasTog, tazasTocda, tazasTocdta, tazasTotonaladas;
-    }
-    public double cdaToUnidades (double cda){
-        double cdaTokg = cda/ 66.6667;
-        double cdaTolb = cda/ 30.6667;
-        double cdaTooz = cda/ 0.5333;
-        double cdaTotaza = cda/ 16;
-        double cdaTog = cda * 15;
-        double cdaTocdta = cda * 3;
-        double cdaTotoneladas = cda / 666666.7
-        return cdaTokg, cdaTolb, cdaTooz, cdaTotaza, cdaTog, cdaTocdta, cdaTotoneladas;
-    }
-    public double cdtaToUnidades (double cdta){
-        double cdtaTokg = cdta / 200;
-        double cdtaTolb = cdta / 92;
-        double cdtaTooz = cdta / 0.1667;
-        double cdtaTotazas = cdta / 48;
-        double cdtaTocda = cdta / 3;
-        double cdtaTog = cdta *5;
-        double cdtaTotoneladas = cdta / 200000;
-        return cdtaTokg, cdtaTolb, cdtaTooz,cdtaTotazas, cdtaTocda, cdtaTog, cdtaTotoneladas;
-    }
-    public double toneladasToUnidades (double toneladas){
-        double toneldasTokg = toneladas * 1000;
-        double toneldasTolb = toneladas * 2204.62;
-        double toneldasTooz = toneladas * 35274;
-        double toneldasTotaza = toneladas * 7812.5;
-        double toneldasTocda = toneladas * 66666.7;
-        double toneldasTocdta = toneladas * 200000;
-        double toneldasTog = toneladas * 2000000;
-        return toneldasTokg, toneldasTolb, toneldasTooz, toneldasTotaza, toneldasTocda, toneldasTocdta, toneldasTog;
-    }
-    public static void main(String[] args) {
-        System.out.println("es"+ toneladasToUnidades);
-    }
 }
