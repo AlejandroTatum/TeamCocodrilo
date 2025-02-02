c86f3717fb24b69b699ff2a1cc5b8bc57bc67bbd
AlejandroTatum <aepadillaespinoza@gmail.com>
2025-02-02 14:17:01 -0500

    ProyectoAlejandro

---
+++ ProyectoFinalPadillaAlejandro/src/proyecto/Controlador.java
@@ -0,0 +1,130 @@
+package proyecto;
+
+import javax.swing.JFileChooser;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alejandro Padilla
+ */
+public class Controlador {
+
+    private ConversionResult[] conversor;
+    private float[] ingredientes = {500, 325, 125, 15, 10, 45};
+    private String[] medida = {"gr", "ml", "gr", "gr", "gr", "ml"};
+    private String[] nombres = {"Harina de trigo", "Agua", "Azúcar", "Levadura seca", "Azúcar", "Aceite de oliva"};
+    private String[] liquidIngredients = {"Agua", "Aceite de oliva"};
+
+    public Controlador() {
+        this.conversor = new ConversionResult[ingredientes.length];
+        for (int i = 0; i < conversor.length; i++) {
+            conversor[i] = new ConversionResult();
+        }
+    }
+
+    public Controlador(float[] ingredientes, String[] medida, String[] nombres) {
+        this.ingredientes = ingredientes;
+        this.medida = medida;
+        this.nombres = nombres;
+    }
+
+    public void metodoElegir() {
+        JFileChooser fc = new JFileChooser();
+        int result = fc.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line;
+                int index = 0;
+
+                while ((line = br.readLine()) != null && index < ingredientes.length) {
+                    try {
+                        ingredientes[index] = Float.parseFloat(line.trim());
+                        index++;
+                    } catch (NumberFormatException e) {
+                        System.out.println("No se pudo convertir: " + line);
+                    }
+                }
+                JOptionPane.showMessageDialog(fc, "guardado correctamente");
+            } catch (IOException e) {
+                System.err.println("Error al leer el archivo: " + e.getMessage());
+            }
+        } else {
+            System.out.println("No se seleccionó ningún archivo.");
+        }
+
+    }
+
+    public float[] getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(float[] ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String[] getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String[] medida) {
+        this.medida = medida;
+    }
+
+    public String[] getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String[] nombres) {
+        this.nombres = nombres;
+    }
+
+    public void metodoModificarIngrediente(int index, float nuevaCantidad) {
+        float cantidadOriginal = ingredientes[index];
+        float factorEscala = nuevaCantidad / cantidadOriginal;
+        ingredientes[index] = nuevaCantidad;
+        for (int i = 0; i < ingredientes.length; i++) {
+            if (i != index) {
+                ingredientes[i] *= factorEscala;
+            }
+            if (!metodoLiquidos(i)) {
+                conversor[i].metodoGramostoUnidades(ingredientes[i]);
+            } else {
+                conversor[i].metodoMililitrostoUnidades(ingredientes[i]);
+            }
+        }
+
+        System.out.println("Receta modificada: " + toString());
+    }
+@Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nombres.length; i++) {
+            String nombre = i < nombres.length ? nombres[i] : "Desconocido";
+            float ingrediente = i < ingredientes.length ? ingredientes[i] : 0.0f;
+            String unidad = i < medida.length ? medida[i] : "unidad";
+            sb.append(String.format("- %s: %.2f %s%n", nombre, ingrediente, unidad));
+            if (i < conversor.length && conversor[i] != null) {
+                sb.append("  ").append(conversor[i].toString()).append(System.lineSeparator());
+            }
+        }
+
+        return sb.toString();
+        //valor mas nombre 
+    }
+
+    public boolean metodoLiquidos(int index) {
+        for (String liquid : liquidIngredients) {
+            if (nombres[index].equalsIgnoreCase(liquid)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
---
+++ ProyectoFinalPadillaAlejandro/src/proyecto/FrmPan.java
@@ -0,0 +1,411 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+import com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author yo
+ */
+public class FrmPan extends javax.swing.JDialog {
+
+    Controlador cr = new Controlador();
+    int posicion;
+    String[] nombres = cr.getNombres();
+    float[] cantidades = cr.getIngredientes();
+    String[] unidades = cr.getMedida();
+
+    public FrmPan(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        metodoLlenarTabla();
+        cr.getIngredientes();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg_ingredientespcf = new javax.swing.ButtonGroup();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        txtRecetaFinal = new javax.swing.JTextArea();
+        btntxt = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tblDatos = new javax.swing.JTable();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tbtModificar = new javax.swing.JTable();
+        btnModificar = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        jMenuItem2 = new javax.swing.JMenuItem();
+
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel1.setText("Pan Casero");
+
+        jLabel2.setText("Seleccione el ingrediente a modificar:");
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "RECETA MODIFICADA", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.ABOVE_TOP, new java.awt.Font("Tahoma", 2, 12))); // NOI18N
+
+        txtRecetaFinal.setEditable(false);
+        txtRecetaFinal.setColumns(20);
+        txtRecetaFinal.setRows(5);
+        jScrollPane4.setViewportView(txtRecetaFinal);
+
+        btntxt.setText("DESCARGAR (.txt)");
+        btntxt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btntxtActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btntxt, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 713, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btntxt)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        tblDatos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Cantidad", "Ingrendiente", "Medida"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tblDatos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblDatosMouseClicked(evt);
+            }
+        });
+        jScrollPane3.setViewportView(tblDatos);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        tbtModificar.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Cantidad", "Ingrediente", "Medida"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                true, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(tbtModificar);
+
+        btnModificar.setText("MODIFICAR");
+        btnModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnModificarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnModificar)
+                .addGap(20, 20, 20))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
+                .addComponent(btnModificar)
+                .addContainerGap())
+        );
+
+        jMenu1.setText("Archivo");
+
+        jMenuItem1.setText("Leer data");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+        jMenu1.add(jSeparator1);
+
+        jMenuItem2.setText("Salir");
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(180, 180, 180)
+                                .addComponent(jLabel1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel2)))
+                        .addGap(148, 148, 148))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(51, 51, 51)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        cr.metodoElegir();
+        metodoLlenarTabla();
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    public void metodoLlenarTabla() {
+        javax.swing.table.DefaultTableModel model = (javax.swing.table.DefaultTableModel) tblDatos.getModel();
+        model.setRowCount(0);
+        for (int i = 0; i < nombres.length; i++) {
+            model.addRow(new Object[]{cantidades[i], nombres[i], unidades[i]});
+        }
+    }
+
+    private void metodoLlenarDatosTabla() {
+        DefaultTableModel model = (DefaultTableModel) tblDatos.getModel();
+        float[] ingredientes = new float[model.getRowCount()];
+        String[] medida = new String[model.getRowCount()];
+        String[] nombres = new String[model.getRowCount()];
+        for (int i = 0; i < model.getRowCount(); i++) {
+            String nombre = (String) model.getValueAt(i, 1); // Columna 1: Ingrediente
+            String cantidadStr = model.getValueAt(i, 0).toString(); // Columna 0: Cantidad
+            String unidad = (String) model.getValueAt(i, 2);
+            Float cantidad = 0.0f;
+            try {
+                cantidad = Float.parseFloat(cantidadStr);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format for cantidad: " + cantidadStr);
+            }
+            ingredientes[i] = cantidad;
+            medida[i] = unidad != null ? unidad : "";
+            nombres[i] = nombre;
+        }
+
+    }
+
+    private void metodoNuevaReceta() {
+        DefaultTableModel modelModificar = (DefaultTableModel) tbtModificar.getModel();
+        String data = modelModificar.getValueAt(0, 0).toString();
+
+        if (data != null && !data.isEmpty()) {
+            try {
+                float number = Float.parseFloat(data);
+                if (number > 0) {
+                    float nuevaCantidad = Float.parseFloat(data);
+                    cr.metodoModificarIngrediente(posicion, nuevaCantidad);
+                    txtRecetaFinal.setText(cr.toString());
+                    JOptionPane.showMessageDialog(this, "Receta actualizada correctamente.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null, "El número debe ser positivo.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "El valor ingresado no es un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "El campo no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+
+        }
+
+    }
+
+    private void metodoRecetaTxt() {
+        String contenido = txtRecetaFinal.getText();
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Guardar como");
+        fileChooser.setSelectedFile(new File("receta.txt"));
+        int result = fileChooser.showSaveDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File archivo = fileChooser.getSelectedFile();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo))) {
+                writer.write(contenido);
+                JOptionPane.showMessageDialog(this, "Archivo guardado exitosamente.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, "Error al guardar el archivo.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void metodoSeleccionarFila() {
+        int selectedRow = tblDatos.getSelectedRow(); // Obtener el índice de la fila seleccionada
+        posicion = selectedRow;
+
+        if (selectedRow != -1) {
+            String cantidadStr = tblDatos.getValueAt(selectedRow, 0).toString();  // Columna 0: Cantidad
+            String nombre = (String) tblDatos.getValueAt(selectedRow, 1);  // Columna 1: Nombre (Ingrediente)
+            String unidad = (String) tblDatos.getValueAt(selectedRow, 2);  // Columna 2: Unidad
+            float cantidad = 0.0f;
+            try {
+                cantidad = Float.parseFloat(cantidadStr);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Error", "Ingresa bien el numero pendejo", JOptionPane.ERROR_MESSAGE);
+            }
+            DefaultTableModel modelModificar = (DefaultTableModel) tbtModificar.getModel();
+            modelModificar.setRowCount(0);
+            modelModificar.addRow(new Object[]{cantidad, nombre, unidad});
+
+        }
+    }
+    private void btnModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarActionPerformed
+        metodoLlenarDatosTabla();
+        metodoNuevaReceta();
+    }//GEN-LAST:event_btnModificarActionPerformed
+
+    private void tblDatosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblDatosMouseClicked
+        metodoSeleccionarFila();
+        metodoNuevaReceta();
+        tbtModificar.setValueAt(0, 0, 0);
+    }//GEN-LAST:event_tblDatosMouseClicked
+
+    private void btntxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btntxtActionPerformed
+        metodoRecetaTxt();
+    }//GEN-LAST:event_btntxtActionPerformed
+
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel(new WindowsClassicLookAndFeel());
+        } catch (Exception e) {
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                FrmPan dialog = new FrmPan(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+
+            }
+        });
+    }
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup bg_ingredientespcf;
+    private javax.swing.JButton btnModificar;
+    private javax.swing.JButton btntxt;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JTable tblDatos;
+    private javax.swing.JTable tbtModificar;
+    private javax.swing.JTextArea txtRecetaFinal;
+    // End of variables declaration//GEN-END:variables
+}
